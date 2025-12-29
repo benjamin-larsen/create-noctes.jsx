568f1708c49c8c9d22f29f7da8bd9124f075b620
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2025-12-29 17:44:05 +0100

    Update

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,5 +1,4 @@
 import HelloWorld from './components/HelloWorld.jsx'
-import JavascriptLogo from './assets/javascript.svg'
 
 export default {
   render() {
@@ -10,7 +9,7 @@ export default {
             <img src='/vite.svg' class='logo' alt='Vite logo' />
           </a>
           <a href='https://github.com/benjamin-larsen/Noctes.jsx' target='_blank'>
-            <img src={JavascriptLogo} class='logo vanilla' alt='JavaScript logo' />
+            <img src='./assets/javascript.svg' class='logo vanilla' alt='JavaScript logo' />
           </a>
         </div>
         <HelloWorld msg='Vite + Noctes.jsx' />
